20a9b779b626c9ba8529296779510c66c7b31fad
Ryan Daley <rpdaley@gmail.com>
2017-04-11 21:11:42 +0000

    E2 done

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,10 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+
+@store1.name = 'Store Name 1'
+@store1.save
+
+puts Store.find(1).name
